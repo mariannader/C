fb7b2ae89cd71fed7cd1e2b91a53a0bb8555b02f
Marian <73747488+mariannader@users.noreply.github.com>
2021-04-16 18:12:25 +0200

    simple test for c programs

---
+++ 1234567/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    printf("the c program to see the 1234567 \n\a");
+    int i ;
+    for (i=1;i<=7;i++)
+    {
+        printf("%d\n",i);
+    }
+
+
+
+    return 0;
+}
